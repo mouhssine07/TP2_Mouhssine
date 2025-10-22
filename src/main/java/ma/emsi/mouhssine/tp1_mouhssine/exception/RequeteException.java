ea567b04a3c84a5fccf289689326580ad75fa7be
Mouhssine <jaibamohsin1@gmail.com>
2025-10-22 10:20:50 +0100

    Création classe RequeteException

---
+++ src/main/java/ma/emsi/mouhssine/tp1_mouhssine/exception/RequeteException.java
@@ -0,0 +1,12 @@
+package ma.emsi.mouhssine.tp1_mouhssine.exception;
+
+public class RequeteException extends Exception {
+
+    public RequeteException(String message) {
+        super(message);
+    }
+
+    public RequeteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
