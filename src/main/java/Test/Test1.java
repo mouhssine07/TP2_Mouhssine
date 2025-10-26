2cd886d2be00ea6bb3c0b70bff52df9ec0ee7f39
Mouhssine <jaibamohsin1@gmail.com>
2025-10-26 19:27:12 +0100

    ajoute de test 1  qui pose une question simple et affiche la réponse de l'API du LLM.n

--- src/main/java/Test/Test1.java
+++ src/main/java/Test/Test1.java
@@ -2,23 +2,20 @@ package Test;
 
 
 import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.chat.request.ResponseFormat;
+
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 
-import java.time.Duration;
 
 
 public class Test1 {
 
     public static void main(String[] args) {
-        // Clé API depuis variable d'environnement
         String apiKey = System.getenv("GEMINI_API_KEY");
         if (apiKey == null) {
-            System.err.println("Variable d'environnement GEMINI_API_KEY non définie !");
+            System.err.println("GEMINIKEY non définie !");
             return;
         }
 
-        // Création du modèle Gemini 2.5 Flash (température 0.7)
         ChatModel model = GoogleAiGeminiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName("gemini-2.5-flash")
@@ -26,7 +23,7 @@ public class Test1 {
                 .build();
 
         // Pose une question simple
-        String question = "Quelle est la capitale du Maroc ?";
+        String question = "Quelle est la capitale du canada ?";
         String reponse = model.chat(question);
 
         // Affiche la réponse
